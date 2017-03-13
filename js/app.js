9561f74c5fa2c6878cf8558d7cacda11bb8d20b8
cutiegirl526 <kayladiniega@gmail.com>
2017-03-13 08:31:12 -1000

    obviously don't follow along well....

--- js/app.js
+++ js/app.js
@@ -1,1 +1,9 @@
 console.log("sanity check");
+
+console.log(document.getElementById("use-id-method"));
+var idElement = document.getElementById("use-id-method");
+console.log(idElement);
+
+console.log(document.getElementsByTagName("div"));
+var tagName = document.getElementsByTagName("div");
+console.log(tagName);
